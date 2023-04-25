cec0a397409e28ecd25b1409b7786f6acf170eb9
jonathan5150 <jonathanmichael5150@gmail.com>
2023-04-24 20:30:32 -0500

    Setup basic spring boot project

---
+++ alongtheway-backend/alongtheway-backend/build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+	java
+	id("org.springframework.boot") version "2.7.11"
+	id("io.spring.dependency-management") version "1.0.15.RELEASE"
+}
+
+group = "com.alongtheway"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_11
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ alongtheway-backend/alongtheway-backend/src/main/java/com/alongtheway/alongthewaybackend/AlongthewayBackendApplication.java
@@ -0,0 +1,13 @@
+package com.alongtheway.alongthewaybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlongthewayBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlongthewayBackendApplication.class, args);
+	}
+
+}
