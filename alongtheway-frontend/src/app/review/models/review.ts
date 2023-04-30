081d54528c1eb5b6419ef68295d004af6eb24d23
wtwente20 <100497007+wtwente20@users.noreply.github.com>
2023-04-30 18:09:01 -0500

    first steps toward image upload

---
+++ alongtheway-frontend/src/app/review/models/review.ts
@@ -0,0 +1,15 @@
+export interface Review {
+  id: number;
+  user: User;
+  trip: Trip;
+  reviewText: string;
+  image1: string;
+  image2: string;
+  image3: string;
+  image4: string;
+  image5: string;
+  image6: string;
+  image7: string;
+  image8: string;
+  image9: string;
+}
