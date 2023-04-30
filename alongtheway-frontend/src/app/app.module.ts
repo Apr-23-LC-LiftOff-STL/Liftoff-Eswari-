25f1e6eba056536f62dd12222822c37d2a155c6c
wtwente20 <100497007+wtwente20@users.noreply.github.com>
2023-04-30 17:59:03 -0500

    basic setup for frontend

--- alongtheway-frontend/src/app/app.module.ts
+++ alongtheway-frontend/src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { ReviewCreateComponent } from './review-create/review-create.component';
+import { ReviewListComponent } from './review-list/review-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    ReviewCreateComponent,
+    ReviewListComponent
   ],
   imports: [
     BrowserModule
---
+++ alongtheway-frontend/src/app/review/review-create/review-create.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-review-create',
+  templateUrl: './review-create.component.html',
+  styleUrls: ['./review-create.component.css']
+})
+export class ReviewCreateComponent {
+
+}
