c4c2c56cbe0f5c6939aff36cbf79a08c339f682c
irynaprofile <105556785+irynaprofile@users.noreply.github.com>
2023-04-30 19:50:34 -0500

    Implement weather api

---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule} from '@angular/common/http';
+import { AppComponent } from './app.component';
+import { WeatherComponent } from './weather/weather.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    WeatherComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/weather/weather.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { WeatherService } from '../weather.service';
+
+@Component({
+  selector: 'app-weather',
+  templateUrl: './weather.component.html',
+  styleUrls: ['./weather.component.css']
+})
+export class WeatherComponent implements OnInit {
+  myWeather:any;
+  temperature: number = 0;
+  feelsLikeTemp: number = 0;
+  humidity: number = 0;
+  pressure: number = 0;
+  summary: string = '';
+  iconURL: string = '';
+  city: string = 'Saint Louis';
+  units: string = 'imperial';
+
+  constructor(private weatherService: WeatherService) { }
+
+  ngOnInit(): void {
+    this.weatherService.getweather(this.city, this.units).subscribe({
+
+      next: (res) => {
+        console.log(res);
+        this.myWeather = res;
+        console.log(this.myWeather);
+        this.temperature = this.myWeather.main.temp;
+        this.feelsLikeTemp = this.myWeather.main.feels_like;
+        this.humidity = this.myWeather.main.humidity;
+        this.pressure = this.myWeather.main.pressure;
+        this.summary = this.myWeather.weather[0].main;
+
+        this.iconURL = 'https://openweathermap.org/img/wn/' + this.myWeather.weather[0].icon + '@2x.png';
+      },
+
+      error: (error) => console.log(error.message),
+
+      complete: () => console.info('API call completed')
+    })
+  }
+
+}
