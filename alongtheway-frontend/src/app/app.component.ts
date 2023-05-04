d88b92ca968a12587b0bf6d03cee491a94da1943
wtwente20 <100497007+wtwente20@users.noreply.github.com>
2023-05-03 22:38:22 -0500

    working map, working autocomplete, calculates route, need direction info

--- alongtheway-frontend/src/app/app.component.ts
+++ alongtheway-frontend/src/app/app.component.ts
@@ -8,17 +8,88 @@ import { Loader } from '@googlemaps/js-api-loader';
 })
 export class AppComponent implements OnInit {
   title = 'alongtheway-frontend';
+  startLocation: string = "";
+  endLocation: string = "";
+  map: google.maps.Map | null = null;
+  directionsService: google.maps.DirectionsService | null = null;
+  directionsRenderer: google.maps.DirectionsRenderer | null = null;
+
   ngOnInit(): void {
-    let loader = new Loader({
-      apiKey: 'AIzaSyC7TfA2reVISM_h9MIhhaoUH6FXsopH8ZA'
-  })
-  
-  loader.load().then(() => {
-    new google.maps.Map(document.getElementById("map")!,{
-      center: { lat:51.233334, lng: 6.78333},
-      zoom: 6
-    })
-  });
+    const loader = new Loader({
+      apiKey: 'AIzaSyC7TfA2reVISM_h9MIhhaoUH6FXsopH8ZA',
+      libraries: ['places']
+    });
+
+    loader.load().then(() => {
+      const mapElement = document.getElementById("map");
+      if (mapElement) {
+        this.map = new google.maps.Map(mapElement, {
+          center: { lat: 51.233334, lng: 6.78333 },
+          zoom: 6
+        });
+        this.directionsService = new google.maps.DirectionsService();
+        this.directionsRenderer = new google.maps.DirectionsRenderer({
+          map: this.map
+        });
+        this.initAutocomplete();
+      } else {
+        console.error("Could not find map element");
+      }
+    });
   }
-  
+
+  initAutocomplete(): void {
+    const startInput = document.getElementById("start-input") as HTMLInputElement;
+    const endInput = document.getElementById("end-input") as HTMLInputElement;
+
+    const startAutocomplete = new google.maps.places.Autocomplete(startInput);
+    const endAutocomplete = new google.maps.places.Autocomplete(endInput);
+
+    startAutocomplete.addListener("place_changed", () => {
+      const place = startAutocomplete.getPlace();
+      if (place && place.geometry && place.geometry.location) {
+        this.map?.setCenter(place.geometry.location);
+        this.map?.setZoom(14);
+        this.startLocation = place.formatted_address ?? "";
+        console.log("Start location:", this.startLocation);
+      }
+    });
+
+    endAutocomplete.addListener("place_changed", () => {
+      const place = endAutocomplete.getPlace();
+      if (place && place.geometry && place.geometry.location) {
+        this.map?.setCenter(place.geometry.location);
+        this.map?.setZoom(14);
+        this.endLocation = place.formatted_address ?? "";
+        console.log("End location:", this.endLocation);
+      }
+    });
+  }
+
+  calculateRoute(): void {
+    if (!this.directionsService || !this.directionsRenderer) {
+      console.error("Directions service or renderer not initialized");
+      return;
+    }
+
+    if (!this.startLocation || !this.endLocation) {
+      console.error("Start or end location not set");
+      return;
+    }
+
+    this.directionsService.route(
+      {
+        origin: this.startLocation,
+        destination: this.endLocation,
+        travelMode: google.maps.TravelMode.DRIVING
+      },
+      (result, status) => {
+        if (status === google.maps.DirectionsStatus.OK) {
+          this.directionsRenderer?.setDirections(result);
+        } else {
+          console.error("Directions request failed:", status);
+        }
+      }
+    );
+  }
 }
