da4dcae5d1b7fab4e957c71a7f0dacc7f3151963
wtwente20 <100497007+wtwente20@users.noreply.github.com>
2023-05-03 22:00:18 -0500

    made working map utilizing google maps

--- alongtheway-frontend/src/app/app.component.ts
+++ alongtheway-frontend/src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Loader } from '@googlemaps/js-api-loader';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'alongtheway-frontend';
+  ngOnInit(): void {
+    let loader = new Loader({
+      apiKey: 'AIzaSyC7TfA2reVISM_h9MIhhaoUH6FXsopH8ZA'
+  })
+  
+  loader.load().then(() => {
+    new google.maps.Map(document.getElementById("map")!,{
+      center: { lat:51.233334, lng: 6.78333},
+      zoom: 6
+    })
+  });
+  }
+  
 }
