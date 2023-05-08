7bb833db9b756a1dda888e92c27d5f51dc375cb1
inkeejeon <inkeejeon@gmail.com>
2023-05-08 18:09:43 -0500

    Updated branch

---
+++ alongtheway-frontend/src/app/aboutusandfaq/aboutusandfaq.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-aboutusandfaq',
+  templateUrl: './aboutusandfaq.component.html',
+  styleUrls: ['./aboutusandfaq.component.css']
+})
+export class AboutusandfaqComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- alongtheway-frontend/src/app/app.module.ts
+++ alongtheway-frontend/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { AboutusandfaqComponent } from './aboutusandfaq/aboutusandfaq.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    AboutusandfaqComponent
   ],
   imports: [
     BrowserModule
