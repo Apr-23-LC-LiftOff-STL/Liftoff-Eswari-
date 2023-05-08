8e4e1cdcddcea763d235b3c276f342cee8a9093a
inkeejeon <inkeejeon@gmail.com>
2023-05-08 18:27:55 -0500

    added component files

--- alongtheway-frontend/src/app/app.module.ts
+++ alongtheway-frontend/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { ProfilepageComponent } from './profilepage/profilepage.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    ProfilepageComponent
   ],
   imports: [
     BrowserModule
