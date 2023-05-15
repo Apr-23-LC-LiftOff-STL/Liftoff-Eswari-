9e7f5b2a3763b32ec42b8801fe95447602686233
jonathan5150 <jonathanmichael5150@gmail.com>
2023-05-15 18:18:45 -0500

    Just added some styling to the search results thing. Page is styled decently now, on to the POIs along the way part.

--- alongtheway-frontend/src/app/app.module.ts
+++ alongtheway-frontend/src/app/app.module.ts
@@ -2,13 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
     HeaderComponent
   ],
   imports: [
