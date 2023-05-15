71deb12d2342352ec3e36b047bf96d84da59c6dc
jonathan5150 <jonathanmichael5150@gmail.com>
2023-05-15 14:58:23 -0500

    FINALLY added autocomplete to my places search inputs. FEELS SO GOOD WHY WAS THIS SO DIFFICULT

--- alongtheway-frontend/src/app/app.component.ts
+++ alongtheway-frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+/// <reference types="@types/googlemaps" />
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 declare const google: any;
 
 @Component({
@@ -6,23 +7,39 @@ declare const google: any;
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit, AfterViewInit {
+export class AppComponent implements OnInit {
   map: any;
-  @ViewChild('mapElement') mapElement: any;
-  @ViewChild('origin', { static: true }) origin: any;
-  @ViewChild('destination', { static: true }) destination: any;
+  @ViewChild('mapElement') mapElement!: ElementRef;
+  originAutocomplete: any;
+  destinationAutocomplete: any;
 
   constructor() {}
 
-  ngAfterViewInit(): void {
+  ngOnInit(): void {
     // Add an event listener to trigger the creation of the map when the Google Maps JavaScript API is loaded
     window.addEventListener('load', () => {
       this.map = new google.maps.Map(this.mapElement.nativeElement, {
         center: { lat: 42.46841179611775, lng: -98.56109182732429 },
         zoom: 4,
       });
+
+      this.initAutocomplete();
     });
   }
 
-  ngOnInit(): void {}
+  initAutocomplete(): void {
+    // Create Autocomplete instances for the origin and destination input fields
+    this.originAutocomplete = new google.maps.places.Autocomplete(
+      document.getElementById('origin'),
+      { types: ['geocode'] }
+    );
+    this.destinationAutocomplete = new google.maps.places.Autocomplete(
+      document.getElementById('destination'),
+      { types: ['geocode'] }
+    );
+  }
+
+  search(): void {
+    // Perform your search logic here
+  }
 }
--- alongtheway-frontend/src/app/app.module.ts
+++ alongtheway-frontend/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
@@ -12,7 +12,8 @@ import { HeaderComponent } from './header/header.component';
     HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
