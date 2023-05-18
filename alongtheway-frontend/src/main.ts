274f2ba5846be98ee37142a50e510aaf40d78b1e
wtwente20 <100497007+wtwente20@users.noreply.github.com>
2023-05-18 17:34:47 -0500

    current working model added to master

--- alongtheway-frontend/src/main.ts
+++ alongtheway-frontend/src/main.ts
@@ -4,9 +4,14 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+if (environment.nodeOptions) {
+  (window as any).NODE_OPTIONS = environment.nodeOptions;
+}
+
 if (environment.production) {
   enableProdMode();
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
