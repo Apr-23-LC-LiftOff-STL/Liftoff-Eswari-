f72fd4c54fc7722342df7e9aabe0b3d6c42e946a
wtwente20 <100497007+wtwente20@users.noreply.github.com>
2023-05-18 22:42:35 -0500

    remove .env

--- alongtheway-frontend/src/environments/environment.ts
+++ alongtheway-frontend/src/environments/environment.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: false, // Set this to true in environment.prod.ts
   mapApiKey: 'AIzaSyCvY26AaAP_iaSPLTIriT65H9UUqKnpA9M',
   mapUrl: "https://maps.googleapis.com/maps/api/js?key=AIzaSyC7TfA2reVISM_h9MIhhaoUH6FXsopH8ZA&libraries=places",
-  weatherApiKey: "4244c81b998ab5fd5f832b68a02c2815",
-  nodeOptions: '--openssl-legacy-provider'
+  weatherApiKey: "4244c81b998ab5fd5f832b68a02c2815"
+  // nodeOptions: '--openssl-legacy-provider'
 };
