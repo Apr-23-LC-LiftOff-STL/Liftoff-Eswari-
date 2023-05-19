59879fa4ebfd208f1c996c604b532f3038f377db
Ian Brey <ianbrey@Ians-iMac.local>
2023-05-18 17:29:08 -0700

    Merged in MongoDB branch. Also removed some depreciated dependencies.

--- alongtheway-backend/alongtheway-backend/build.gradle.kts
+++ alongtheway-backend/alongtheway-backend/build.gradle.kts
@@ -16,11 +16,9 @@ dependencies {
 	implementation ("org.mongodb:mongodb-driver-sync")
 	implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
 	implementation ("org.springframework.boot:spring-boot-starter-thymeleaf")
-//	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation ("org.springframework.security:spring-security-crypto")
 	implementation ("org.springframework.boot:spring-boot-starter-validation")
-//	implementation ("mysql:mysql-connector-java:8.0.29")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
