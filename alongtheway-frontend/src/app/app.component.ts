5e3197226968bc290a6aaff59d2d489a44f40925
unknown <jonathanmichael5150@gmail.com>
2023-05-28 01:57:16 -0500

    Added popup boxes when you click icons on the map. Updated the parameters to show the hardcoded options correctly. This code currently will search within 10 miles of the route, and display only restaurants that have a 4 star or better rating. Planning on fixing the list part and the paramaters linkage in the next 2 days.

--- alongtheway-frontend/src/app/app.component.ts
+++ alongtheway-frontend/src/app/app.component.ts
@@ -18,6 +18,8 @@ export class AppComponent implements OnInit {
   PolygonBound: any;
   service!: google.maps.places.PlacesService;
   places: any[] = [];
+  openInfoWindow: google.maps.InfoWindow | null = null; // Declare the openInfoWindow property
+
 
   ngOnInit(): void {
     window.addEventListener('load', () => {
@@ -64,30 +66,30 @@ export class AppComponent implements OnInit {
           this.directionsRenderer.setDirections(response);
           waypoints = polyline.decode(response.routes[0].overview_polyline) as number[][];
 
-          const PolygonCoords = this.PolygonPoints();
-          const newPolygonBound = new google.maps.Polygon({
-            paths: PolygonCoords,
-            strokeColor: "#FF0000",
-            strokeOpacity: 0.8,
-            strokeWeight: 2,
-            fillColor: "#FF0000",
-            fillOpacity: 0.35,
-          });
-
-          // Clear the previous PolygonBound from the map
-          if (this.PolygonBound) {
-            this.PolygonBound.setMap(null);
-          }
-
-          // Assign the new PolygonBound to this.PolygonBound
-          this.PolygonBound = newPolygonBound;
-
-          // Display the new PolygonBound on the map
-          this.PolygonBound.setMap(this.map);
+//           const PolygonCoords = this.PolygonPoints();
+//           const newPolygonBound = new google.maps.Polygon({
+//             paths: PolygonCoords,
+//             strokeColor: "#FF0000",
+//             strokeOpacity: 0.8,
+//             strokeWeight: 2,
+//             fillColor: "#FF0000",
+//             fillOpacity: 0.35,
+//           });
+//
+//           // Clear the previous PolygonBound from the map
+//           if (this.PolygonBound) {
+//             this.PolygonBound.setMap(null);
+//           }
+//
+//           // Assign the new PolygonBound to this.PolygonBound
+//           this.PolygonBound = newPolygonBound;
+//
+//           // Display the new PolygonBound on the map
+//           this.PolygonBound.setMap(this.map);
 
           this.service = new google.maps.places.PlacesService(this.map);
 
-          this.searchPlacesNearWaypoints(); // Call the function to search for places near waypoints
+          this.searchPlacesAlongRoute(); // Call the function to search for places near waypoints
 
         } else {
           window.alert('Directions request failed due to ' + status);
@@ -96,78 +98,102 @@ export class AppComponent implements OnInit {
     );
   }
 
-  PolygonPoints(): typeof FullPoly {
-    let polypoints = waypoints;
-    let PolyLength = polypoints.length;
+//   PolygonPoints(): typeof FullPoly {
+//     let polypoints = waypoints;
+//     let PolyLength = polypoints.length;
+//
+//     let UpperBound: google.maps.LatLngLiteral[] = [];
+//     let LowerBound: google.maps.LatLngLiteral[] = [];
+//
+//     for (let j = 0; j <= PolyLength - 1; j++) {
+//       let NewPoints = this.PolygonArray(polypoints[j][0]);
+//       UpperBound.push({ lat: NewPoints[0], lng: polypoints[j][1] });
+//       LowerBound.push({ lat: NewPoints[1], lng: polypoints[j][1] });
+//     }
+//     let reversebound = LowerBound.reverse();
+//
+//     let FullPoly = UpperBound.concat(reversebound);
+//
+//     return FullPoly;
+//   }
+//
+//   PolygonArray(latitude: number): number[] {
+//     const R = 6378137;
+//     const pi = 3.14;
+//     //distance in meters
+//     const selectElement = document.getElementById('distance') as HTMLSelectElement;
+//     const selectedValue = parseInt(selectElement.value); // Get the selected value as an integer
+//
+//     // Update the upper_offset and lower_offset based on the selected value
+//     const upper_offset = parseFloat(selectElement.value) * 1609.34; // 1 mile is approximately 1609.34 meters
+//     const lower_offset = -upper_offset;
+//
+//     const Lat_up = upper_offset / R;
+//     const Lat_down = lower_offset / R;
+//     //OffsetPosition, decimal degrees
+//     const lat_upper = latitude + (Lat_up * 180) / pi;
+//     const lat_lower = latitude + (Lat_down * 180) / pi;
+//
+//     return [lat_upper, lat_lower];
+//   }
 
-    let UpperBound: google.maps.LatLngLiteral[] = [];
-    let LowerBound: google.maps.LatLngLiteral[] = [];
 
-    for (let j = 0; j <= PolyLength - 1; j++) {
-      let NewPoints = this.PolygonArray(polypoints[j][0]);
-      UpperBound.push({ lat: NewPoints[0], lng: polypoints[j][1] });
-      LowerBound.push({ lat: NewPoints[1], lng: polypoints[j][1] });
-    }
-    let reversebound = LowerBound.reverse();
 
-    let FullPoly = UpperBound.concat(reversebound);
+searchPlacesAlongRoute(): void {
+  const radiusMiles = 10; // Set the radius for searching places in miles
+  const minimumRating = 4; // Minimum rating to include in the results
 
-    return FullPoly;
-  }
+  const route = this.directionsRenderer.getDirections().routes[0];
+  const overviewPath = route.overview_path;
 
-  PolygonArray(latitude: number): number[] {
-    const R = 6378137;
-    const pi = 3.14;
-    //distance in meters
-    const selectElement = document.getElementById('distance') as HTMLSelectElement;
-    const selectedValue = parseInt(selectElement.value); // Get the selected value as an integer
+  overviewPath.forEach((point: google.maps.LatLng) => {
+    const location = point;
 
-    // Update the upper_offset and lower_offset based on the selected value
-    const upper_offset = parseFloat(selectElement.value) * 1609.34; // 1 mile is approximately 1609.34 meters
-    const lower_offset = -upper_offset;
+    const request = {
+      location: location,
+      radius: radiusMiles * 1609.34, // Convert miles to meters
+      type: 'restaurant' // Set the type of place you want to search for
+    };
 
-    const Lat_up = upper_offset / R;
-    const Lat_down = lower_offset / R;
-    //OffsetPosition, decimal degrees
-    const lat_upper = latitude + (Lat_up * 180) / pi;
-    const lat_lower = latitude + (Lat_down * 180) / pi;
+    this.service.nearbySearch(request, (results: any[], status: any) => {
+      if (status === google.maps.places.PlacesServiceStatus.OK) {
+        // Filter results based on minimum rating
+        const filteredResults = results.filter(place => place.rating >= minimumRating);
 
-    return [lat_upper, lat_lower];
-  }
+        // Process the search results here
+        this.updatePlacesList(filteredResults); // Update the places list with the filtered results
 
-  searchPlacesNearWaypoints(): void {
-    const radiusMiles = 10; // Set the radius for searching places in miles
-
-    waypoints.forEach((waypoint: number[]) => {
-      const lat = waypoint[0];
-      const lng = waypoint[1];
-
-      const location = new google.maps.LatLng(lat, lng);
-
-      const request = {
-        location: location,
-        radius: radiusMiles * 1609.34, // Convert miles to meters
-        type: 'restaurant' // Set the type of place you want to search for
-      };
-
-      this.service.nearbySearch(request, (results: any, status: any) => {
-        if (status === google.maps.places.PlacesServiceStatus.OK) {
-          // Process the search results here
-          this.updatePlacesList(results); // Update the places list with the search results
-
-          // Add markers for each place
-          results.forEach((place: any) => {
-            const placeLocation = place.geometry.location;
-            const marker = new google.maps.Marker({
-              position: placeLocation,
-              map: this.map,
-              title: place.name
-            });
+        // Add markers for each place
+        filteredResults.forEach((place: any) => {
+          const placeLocation = place.geometry.location;
+          const marker = new google.maps.Marker({
+            position: placeLocation,
+            map: this.map,
+            title: place.name
           });
-        }
-      });
+
+          // Create an info window for the marker
+          const infoWindow = new google.maps.InfoWindow({
+            content: `<strong>${place.name}</strong><br>${place.vicinity}`
+          });
+
+          // Add a click event listener to the marker
+          marker.addListener('click', () => {
+            // Close the previously open info window
+            if (this.openInfoWindow) {
+              this.openInfoWindow.close();
+            }
+
+            // Open the new info window
+            infoWindow.open(this.map, marker);
+            this.openInfoWindow = infoWindow; // Update the currently open info window
+          });
+        });
+      }
     });
-  }
+  });
+}
+
 
   updatePlacesList(results: any[]): void {
     this.places = results;
