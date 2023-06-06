78f666a5cadd99f5574d983d1468e5b54c2e3569
wtwente20 <100497007+wtwente20@users.noreply.github.com>
2023-06-05 20:02:42 -0500

    added auth.service and corrected header

--- alongtheway-frontend/src/app/header/header.component.ts
+++ alongtheway-frontend/src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  username: string | null = null;
 
-  constructor() { }
+  constructor(public authService: AuthService) { }
 
   ngOnInit(): void {
+    this.authService.getUsername.subscribe(username => {
+      this.username = username;
+    });
   }
 
+  logout(): void {
+    this.authService.logout();
+  }
 }
