cd2ea7f59838bbb7dc87e5ef23769075db736364
wtwente20 <100497007+wtwente20@users.noreply.github.com>
2023-06-05 19:32:41 -0500

    small commit

---
+++ alongtheway-backend/alongtheway-backend/src/main/java/com/alongtheway/alongthewaybackend/security/TokenProvider.java
@@ -0,0 +1,55 @@
+package com.alongtheway.alongthewaybackend.security;
+
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+@Component
+public class TokenProvider {
+
+    @Value("${app.jwt.secret}") // Read the secret key from application properties
+    private String secretKey;
+
+    @Value("${app.jwt.expiration}") // Read the expiration time from application properties
+    private long expirationTime;
+
+    public String generateToken(String username) {
+        Date now = new Date();
+        Date expiryDate = new Date(now.getTime() + expirationTime);
+
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(expiryDate)
+                .signWith(SignatureAlgorithm.HS512, secretKey)
+                .compact();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public String getUsernameFromToken(String token) {
+        Claims claims = Jwts.parser()
+                .setSigningKey(secretKey)
+                .parseClaimsJws(token)
+                .getBody();
+
+        return claims.getSubject();
+    }
+
+    public long getExpirationTime() {
+        return expirationTime;
+    }
+}
+
