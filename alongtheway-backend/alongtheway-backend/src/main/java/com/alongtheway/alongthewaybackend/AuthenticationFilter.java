78adf00c9bf5e367f54c197ea7a916fd49fa7ce1
Andrewfischer100 <60162693+Andrewfischer100@users.noreply.github.com>
2023-06-07 16:55:19 -0500

    test

--- alongtheway-backend/alongtheway-backend/src/main/java/com/alongtheway/alongthewaybackend/AuthenticationFilter.java
+++ alongtheway-backend/alongtheway-backend/src/main/java/com/alongtheway/alongthewaybackend/AuthenticationFilter.java
@@ -13,7 +13,7 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 
-public class AuthenticationFilter extends HandlerInterceptorAdapter {
+public class  AuthenticationFilter extends HandlerInterceptorAdapter {
 
     @Autowired
     UserRepository userRepository;
