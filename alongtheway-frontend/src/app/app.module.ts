9babc28d8cd888955654859b9ea529f1bc32ed6c
wtwente20 <100497007+wtwente20@users.noreply.github.com>
2023-06-07 19:50:50 -0500

    attempting declutter of console errors

--- alongtheway-frontend/src/app/app.module.ts
+++ alongtheway-frontend/src/app/app.module.ts
@@ -1,12 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { HeaderComponent } from './header/header.component';
-import { FormsModule } from '@angular/forms';
-import { WeatherService } from './weather/weather.service';
-import { WeatherComponent } from './weather/weather.component';
-import { HttpClientModule } from '@angular/common/http';
 import { AboutusandfaqComponent } from './aboutusandfaq/aboutusandfaq.component';
 import { AppRoutingModule } from './app-routing.module';
 import { ProfilepageComponent } from './profilepage/profilepage.component';
