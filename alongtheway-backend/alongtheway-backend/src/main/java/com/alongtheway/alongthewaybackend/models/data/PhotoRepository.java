6ef9d4f9af89e744d9639c71bfcdc530167bfd7f
Ian Brey <ianbrey@Ians-iMac.local>
2023-06-08 19:20:00 -0700

    removed everything pertaining to photos

--- alongtheway-backend/alongtheway-backend/src/main/java/com/alongtheway/alongthewaybackend/models/data/PhotoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.alongtheway.alongthewaybackend.models.data;
-
-import com.alongtheway.alongthewaybackend.models.Photo;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface PhotoRepository extends MongoRepository<Photo, String> {
-
-
-}
